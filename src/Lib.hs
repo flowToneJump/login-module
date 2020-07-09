ac0e98265475913314c3164ffea568f5b275ada2
flowToneJump <64518929+flowToneJump@users.noreply.github.com>
2020-07-08 18:28:59 -0700

    Adds saltedHash and hashValidation to Lib

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,52 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-} 
+{-# LANGUAGE TypeApplications #-} 
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib
+  ( saltedHash 
+  , hashValidation 
+  , ValidationErrorType(..)
+  , ValidationError(..)
+  ) where
+
+import qualified Crypto.KDF.BCrypt as BCrypt
+import           Data.Bifunctor (first)
+import           Data.ByteString (ByteString)
+import           Data.List (isPrefixOf)
+import           Data.String.Conv (toS)
+import           Data.Text (Text)
+
+-- | Generate a salted hash for a given text input
+saltedHash
+  :: Text
+  -- ^ Text to hash
+  -> IO Text 
+saltedHash = fmap (toS @ByteString) . BCrypt.hashPassword 12 . toS @_ @ByteString 
+
+-- | Possible outcomes of `hashValidation`
+data ValidationErrorType
+  = InvalidHashFormat 
+  | UnsupportedVersion
+  | InvalidCost 
+  | UnknownError
+    deriving (Show, Eq)
+
+newtype ValidationError
+  = MkValidationError (ValidationErrorType, String)
+    deriving (Show, Eq)
+
+-- | Validate the given text input against a hash
+hashValidation
+  :: Text
+  -- ^ Candidate original text
+  -> Text
+  -- ^ Hash of original text
+  -> Either ValidationError Bool 
+hashValidation password hashed = first wrapError $ BCrypt.validatePasswordEither password' hashed' where 
+  password' = toS @_ @ByteString password
+  hashed' = toS @_ @ByteString hashed
+  wrapError str = MkValidationError (errorType str, str)
+  errorType str
+    | "Invalid hash" `isPrefixOf` str = InvalidHashFormat
+    | "Unsupported" `isPrefixOf` str = UnsupportedVersion
+    | "Invalid by" `isPrefixOf` str = InvalidCost
+    | otherwise = UnknownError
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Lib
+
+import Data.Either (isLeft)
+import Data.Text (Text)
+import Test.Hspec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+
+  describe "Lib.saltedHash" $ do
+    it "Outputs different text from input" $ do
+      let pass = "HelloThere"
+      result <- saltedHash pass
+      result `shouldNotBe` pass
+
+  describe "Lib.hashValidation" $ do
+    it "Accepts associated hash" $ do
+      let pass = "HelloThere"
+      hashed <- saltedHash pass
+      hashValidation pass hashed `shouldBe` Right True
+
+    it "Rejects non-associated hash" $ do
+      let pass = "HelloThere"
+      let other = "Wrong"
+      hashed <- saltedHash other
+      hashValidation pass hashed `shouldBe` Right False 
+
+    it "Returns error on invalid format" $ do
+      let pass = "HelloThere"
+      let hashed = "Wrong"
+      hashValidation pass hashed `shouldSatisfy` isLeft
+

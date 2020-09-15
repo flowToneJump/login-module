09f58d77210db2329bd3ea067c6df1c0c948dcae
Jeffrey Dwyer <jeffreydwyer95@gmail.com>
2020-09-14 19:26:13 -0700

    Implements full JWT validation for Google login

--- src/Web/Crypto/JWT.hs
+++ src/Web/Crypto/JWT.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Web.Crypto.JWT where
 
-import Data.ByteString
+import Control.Monad
+import Data.ByteString (ByteString)
 import Data.Text
 import Web.JWT
+import qualified Web.JWT as JWT
 
 data JwtValidationError
   = PemParsingError
@@ -10,13 +14,30 @@ data JwtValidationError
   | NoPemParsed
   | CouldNotParseJwt
   | InvalidJwt
+  | InvalidAud
+  | InvalidIss
+  | Expired
 
 newtype RawPem = RawPem ByteString
 
 newtype RawJwt = RawJwt Text
 
-verifyJwt :: RawPem -> RawJwt -> Either JwtValidationError (JWT VerifiedJWT)
-verifyJwt (RawPem rawPem) (RawJwt rawJwt) = do
+data GoogleAuthInfo = GoogleAuthInfo
+  { appClientId :: StringOrURI,
+    validIssOpts :: [StringOrURI],
+    currTime :: NumericDate
+  }
+
+verifyJwt :: RawPem -> RawJwt -> GoogleAuthInfo -> Either JwtValidationError (JWT VerifiedJWT)
+verifyJwt (RawPem rawPem) (RawJwt rawJwt) authInfo = do
   pem <- maybe (Left PemParsingError) pure $ readRsaSecret rawPem
   unverifiedJwt <- maybe (Left CouldNotParseJwt) pure $ decode rawJwt
-  maybe (Left InvalidJwt) pure $ verify (RSAPrivateKey pem) unverifiedJwt
+  jwt <- maybe (Left InvalidJwt) pure $ verify (RSAPrivateKey pem) unverifiedJwt
+  unless (validAud jwt) $ Left InvalidAud
+  unless (validIss jwt) $ Left InvalidIss
+  unless (not $ expired jwt) $ Left InvalidIss
+  pure jwt
+  where
+    validAud = maybe False (elem (appClientId authInfo) . either pure id) . aud . claims
+    validIss = maybe False (flip elem (validIssOpts authInfo)) . iss . claims
+    expired = maybe False (currTime authInfo <=) . JWT.exp . claims
--- src/Web/Login/Google.hs
+++ src/Web/Login/Google.hs
@@ -1,15 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+
 module Web.Login.Google (validateGoogleLoginToken, GoogleLoginToken (..)) where
 
 import Data.ByteString
 import Data.Text
+import Network.HTTP.Req
 import Web.Crypto.JWT
 
 googlePem :: IO ByteString
-googlePem = undefined
+googlePem = runReq config $ do
+  responseBody <$> req GET url NoReqBody bsResponse mempty
+  where
+    config = defaultHttpConfig
+    url = https "www.googleapis.com" /: "oauth2" /: "v1" /: "certs"
 
 newtype GoogleLoginToken = GoogleLoginToken Text
 
-validateGoogleLoginToken :: GoogleLoginToken -> IO (Either JwtValidationError ())
-validateGoogleLoginToken (GoogleLoginToken jwt) = do
+validateGoogleLoginToken :: GoogleLoginToken -> GoogleAuthInfo -> IO (Either JwtValidationError ())
+validateGoogleLoginToken (GoogleLoginToken jwt) authInfo = do
   pem <- googlePem
-  pure $ () <$ verifyJwt (RawPem pem) (RawJwt jwt)
+  pure $ () <$ verifyJwt (RawPem pem) (RawJwt jwt) authInfo
